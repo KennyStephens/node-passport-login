8dbe180957d4dde2eff0b0cb49bc2d10a5b1528a
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2019-03-13 13:49:03 -0600

    Finished.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const flash = require('connect-flash');
-const session = require('express-session');
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require('passport');
 
 const app = express();
+
+// Passport config
+require('./config/passport')(passport);
 
 // DB Config
 const db = require("./config/keys").MongoURI;
@@ -31,15 +35,20 @@ app.use(
   })
 );
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect Flash
 app.use(flash());
 
 // Global variables
 app.use(function(req, res, next) {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    next();
-  });
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 // Routes
 app.use("/", require("./routes/index"));
